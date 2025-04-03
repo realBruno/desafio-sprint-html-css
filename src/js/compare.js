24d923a0dc5d1b292b3d8f3d966220b512288e38
Bruno <soubruno@protonmail.com>
2025-04-03 15:08:58 -0300

    Allow user to choose only two cars

--- src/js/compare.js
+++ src/js/compare.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const xl = document.querySelector(".xl");
+const xls = document.querySelector(".xls");
+const storm = document.querySelector(".storm");
+
+const allCheckboxes = document.querySelectorAll(".checkbox");
+
+let max = 2;
+
+function moreThanTwo() {
+    alert("ERROR MESSAGE")
+}
+
+function doTheChecking(e) {
+    if (!max && e.checked) {
+        moreThanTwo();
+        e.checked = false;
+    }
+    else e.checked ? max-- : max++;
+}
+
+allCheckboxes.forEach((val) => {
+    val.addEventListener("change", () => {
+        doTheChecking(val);
+    })
+});
