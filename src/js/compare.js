0560ef790abeda1d6e9796a83c0404b5df9d8dee
Bruno <soubruno@protonmail.com>
2025-04-04 03:21:53 -0300

    Small changes

--- src/js/compare.js
+++ src/js/compare.js
@@ -273,12 +273,13 @@ const info = document.querySelectorAll(".info");
 info.forEach((e) => {
     e.addEventListener("click", () => {
         if (e.className.includes("info1")) {
+            changeBackground();
             dialog.showModal();
             dialog.innerHTML = `           
                 <table class="tabela-dialog">
                 <tr>
-                    <th></th>
-                    <th class="image-container"><img class="car car1" src="../../public/images/xl_cabine.jpg" alt=""></th>
+
+                    <th colspan="2" class="image-container"><img class="car car1" src="../../public/images/xl_cabine.jpg" alt=""></th>
                 </tr>
     
                 <tr class="par modelo">
@@ -335,16 +336,16 @@ info.forEach((e) => {
         }
 
         else if(e.className.includes("info2")) {
+            changeBackground();
             dialog.showModal();
-            dialog.innerHTML = `<table class="tabela-dialog">
+            dialog.innerHTML = `  <table class="tabela-dialog">
                 <tr>
-                    <th></th>
-                    <th class="image-container"><img class="car car1" src="../../public/images/xl_cabine.jpg" alt=""></th>
+                    <th colspan="2" class="image-container"><img class="car car1" src="../../public/images/xls_diesel.jpg" alt=""></th>
                 </tr>
     
                 <tr class="par modelo">
                     <td>Modelo</td>
-                    <td>XL Cabine Diesel 4x4 2022</td>
+                    <td>XLS 2.2 Diesel 4X4 AT 2022</td>
                 </tr>
 
                 <tr class="impar">
@@ -364,7 +365,7 @@ info.forEach((e) => {
 
                 <tr class="par">
                     <td>Capacidade de carga (kg)</td>
-                    <td>1234</td>
+                    <td>1076</td>
                 </tr>
 
                 <tr class="impar">
@@ -379,7 +380,7 @@ info.forEach((e) => {
 
                 <tr class="impar">
                     <td>Volume da caçamba (L)</td>
-                    <td>1420</td>
+                    <td>1180</td>
                 </tr>
 
                 <tr class="par">
@@ -389,14 +390,72 @@ info.forEach((e) => {
 
                 <tr class="impar">
                     <td>Preço</td>
-                    <td>R$ 153.376,00</td>
+                    <td>R$ 155.890,00</td>
                 </tr>
             </table>
             <div class="container-fechar"><button class="fechar" onclick="hide()">Fechar</button></div>`
+               
         }
 
         else {
+            changeBackground();
+            dialog.showModal();
+            dialog.innerHTML = `<table class="tabela-dialog">
+                <tr>
+                    <th colspan="2" class="image-container"><img class="car car2" src="../../public/images/storm.jpg" alt=""></th>
+                </tr>
+    
+                <tr class="par modelo">
+                    <td>Modelo</td>
+                    <td>Storm 3.2 Diesel 4X4 AT 2022</td>
+                </tr>
 
+                <tr class="impar">
+                    <td>Altura da caçamba (mm)</td>
+                    <td>511</td>
+                </tr>
+
+                <tr class="par">
+                    <td>Altura do veículo (mm)</td>
+                    <td>1821</td>
+                </tr>
+
+                <tr class="impar">
+                    <td>Altura livre do solo (mm)</td>
+                    <td>232</td>
+                </tr>
+
+                <tr class="par">
+                    <td>Capacidade de carga (kg)</td>
+                    <td>1040</td>
+                </tr>
+
+                <tr class="impar">
+                    <td>Motor</td>
+                    <td>3.2</td>
+                </tr>
+
+                <tr class="par">
+                    <td>Potência (CV)</td>
+                    <td>200</td>
+                </tr>
+
+                <tr class="impar">
+                    <td>Volume da caçamba (L)</td>
+                    <td>1180</td>
+                </tr>
+
+                <tr class="par">
+                    <td>Roda</td>
+                    <td>Liga Leve 17</td>
+                </tr>
+
+                <tr class="impar">
+                    <td>Preço</td>
+                    <td>R$ 169.740,00</td>
+                </tr>
+            </table>
+            <div class="container-fechar"><button class="fechar" onclick="hide()">Fechar</button></div>`
         }
 
     });
